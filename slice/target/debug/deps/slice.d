07c4ddd9b6d940cbc81338df73cf48f2c1f3256e
Jon Borden <jonborden@gmail.com>
2021-07-25 10:30:08 -0500

    rust slice practice

---
+++ slice/target/debug/deps/slice.d
@@ -0,0 +1,5 @@
+C:\development\practice\slice\target\debug\deps\slice.exe: src\main.rs
+
+C:\development\practice\slice\target\debug\deps\slice.d: src\main.rs
+
+src\main.rs:
---
+++ slice/target/debug/slice.d
@@ -0,0 +1,1 @@
+C:\development\practice\slice\target\debug\slice.exe: C:\development\practice\slice\src\main.rs
