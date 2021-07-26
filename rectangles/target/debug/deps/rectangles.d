ecdf3f1b903568f7738e76db3ba0df3e8fc46b5c
Jon Borden <jonborden@gmail.com>
2021-07-26 18:15:41 -0500

    rust rectangles and beyond

---
+++ rectangles/target/debug/deps/rectangles.d
@@ -0,0 +1,5 @@
+C:\development\practice\rectangles\target\debug\deps\rectangles.exe: src\main.rs
+
+C:\development\practice\rectangles\target\debug\deps\rectangles.d: src\main.rs
+
+src\main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+C:\development\practice\rectangles\target\debug\rectangles.exe: C:\development\practice\rectangles\src\main.rs
